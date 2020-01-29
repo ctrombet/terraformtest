df8547540721b49377886c8fae7f863d49ebc462
cristian.trombetta <cristian.trombetta@appway.com>
2020-01-29 10:46:24 +0100

    Ading provider

---
+++ provider.tf
@@ -0,0 +1,6 @@
+# Terraform Azure Provider configuration
+
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.36.1"
+}
